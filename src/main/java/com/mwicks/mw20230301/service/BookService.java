263e3c105244d574ed5bce90c6dc9b1459a5f2b0
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-03-02 13:13:52 -0600

    beginning switch from ArrayMap to HashMap

--- src/main/java/com/mwicks/mw20230301/service/BookService.java
+++ src/main/java/com/mwicks/mw20230301/service/BookService.java
@@ -3,40 +3,41 @@ package com.mwicks.mw20230301.service;
 import com.mwicks.mw20230301.entities.Book;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BookService {
 
-    List<Book> library = new ArrayList<>(Arrays.asList(
+    Map<Integer, Book> libraryMap = new HashMap<>();
+
+    /*List<Book> library = new ArrayList<>(Arrays.asList(
         new Book("The Hundred-Foot Journey", "novel", "jane@doe.com"),
         new Book("Kanji & Kana", "textbook", "rei@netzero.com"),
         new Book("Bible", "historical fiction", "john@baptist.net")
-    ));
+    ));*/
 
-    public List<Book> getAllBooks() {
+    public Map<Integer, Book> getAllBooks() {
         System.out.println("getAllBooks trigger");
-        return library;
+        return libraryMap;
     }
 
     public Book getBookById(int id) {
         System.out.println("getBookById trigger");
-        return library.stream().filter(book -> book.getBookId() == id).findFirst().get();
+        return libraryMap.stream().filter(book -> book.getBookId() == id).findFirst().get();
     }
 
     public Book getBookByName(String name) {
         System.out.println("getBookByName trigger");
-        return library.stream().filter(e -> (e.getBookName().toLowerCase().equals(name.toLowerCase()))).findFirst().get();
+        return libraryMap.stream().filter(e -> (e.getBookName().toLowerCase().equals(name.toLowerCase()))).findFirst().get();
     }
 
     public void addBook(String name, String description, String email) {
         System.out.println("addBook trigger");
-        library.add(new Book(name, description, email));
+        Book newBook = new Book(name,description,email);
+        libraryMap.put(newBook.getBookId(), newBook);
     }
 
     public void deleteBook(int bookId) {
-        library.remove(getBookById(bookId));
+        libraryMap.remove(getBookById(bookId));
     }
 }
